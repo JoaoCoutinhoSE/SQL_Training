230d0d2e9b0924a6594a7e723a659456505c950c
pandazim <jaotoledo74@gmail.com>
2020-02-10 01:13:12 -0400

    Starting CRUD

--- app/src/main/java/com/example/sqlitetraining/Banco_dados.kt
+++ app/src/main/java/com/example/sqlitetraining/Banco_dados.kt
@@ -3,28 +3,48 @@ package com.example.sqlitetraining
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
 
-class Banco_dados(context: Context,
-                  name: String?,
-                  factory: SQLiteDatabase.CursorFactory?,
-                  version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
+object FeedReaderContract {
+    object FeedEntry : BaseColumns {
 
-    private const val SQL_CREATE_ENTRIES = "Create.db"
+         const val TABLE_NAME = "bd_clientes"
+
+         const val COLUMN_NAME = "Codigo"
+         const val COLUMN_CODE = "Nome"
+         const val COLUMN_CELLPHONE = "Telefone"
+         const val COLUMN_EMAIL = "Email"
+
+    }
+}
+
+private const val SQL_CREATE_ENTRIES = "CREATE TABLE ${FeedReaderContract.FeedEntry.TABLE_NAME} (" +
+        "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+        "${FeedReaderContract.FeedEntry.COLUMN_CODE} INTEGER PRIMARY KEY, " +
+        "${FeedReaderContract.FeedEntry.COLUMN_NAME} TEXT, " +
+        "${FeedReaderContract.FeedEntry.COLUMN_CELLPHONE} TEXT, " +
+        "${FeedReaderContract.FeedEntry.COLUMN_EMAIL} TEXT "
+
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME
+
+class Banco_dados(context: Context) : SQLiteOpenHelper(context, database_name, null, database_version) {
+
 
     override fun onCreate(db: SQLiteDatabase?) {
 
+        db?.execSQL(SQL_CREATE_ENTRIES)
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(SQL_DELETE_ENTRIES)
 
     }
 
     companion object {
 
-        const val version = 1
-        const val name = "Banco_dados"
+        const val database_version = 1
+        const val database_name = "bd_clientes"
 
 
     }
